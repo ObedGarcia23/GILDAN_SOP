50c1a427c2fb23c157d523126c1cb2db3c329f23
ObedGarcia23 <69164975+ObedGarcia23@users.noreply.github.com>
2022-12-15 07:59:54 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INGENIERIA SOP</title>
+    
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./index.css">
+</head>
+
+<body>
+
+<!--
+<section style="text-align: center;">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <img src="./src/img/Logo.png" alt="" width="150px">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link linkcolor-" href="#">INICIO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link linkcolor" href="#">Link<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link linkcolor dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Area
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Tejido</a>
+          <a class="dropdown-item" href="#">Teñido</a>
+          <a class="dropdown-item" href="./views/Boarding/">Boarding</a>
+
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link linkcolor" href="./views/Dashboard/">Dashboard</a>
+      </li>
+      
+    </ul>
+    
+    <form class="form-inline my-2 my-lg-0" style="display: flex;">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-md-0" type="submit">LOGOUT</button>
+    </form> 
+  </div>
+</nav>
+</section> 
+-->
+
+<div class="principal">
+  <div class="Formulario">
+    <div class="Test1">
+        <img src="./src/img/Logo.png" alt="" >
+    </div>  
+  <div class="Test2">
+    <h2>INGENIERIA RN3 - RN4</h2>
+
+    </div>  
+      <form action="./views/Home/" method="POST">
+        <label for="user">USUARIO</label>
+        <input type="text" id="user" name="user" placeholder="Usuario..">
+        <label for="pass">Contraseña</label>
+        <input type="password" id="pass" name="pass" placeholder="Contraseña..">
+        <button type="submit" class="btn btn-primary">Ingresar</button>
+      </form>
+
+      <div class="Test3">
+          <img src="./src/img/Ing.png" alt="" >
+      </div>
+    </div>
+  </div>
+            
+
+<!-- Agrega el código de Bootstrap 5 en el Script -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>        
+
+
+</body>
+</html>
---
+++ views/Boarding/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SOP BOARDING</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+    
+
+</head>
+<body>
+<section style="text-align: center;">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <img src="../../src/img/Logo.png" alt="" width="150px">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link linkcolor active" href="../Home/">INICIO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link linkcolor" href="#">Link<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link linkcolor dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Area
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Tejido</a>
+          <a class="dropdown-item" href="#">Teñido</a>
+          <a class="dropdown-item" href="./views/Boarding/">Boarding</a>
+
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link linkcolor" href="./views/Dashboard/">Dashboard</a>
+      </li>
+      
+    </ul>
+    
+    <form class="form-inline my-2 my-lg-0" style="display: flex;">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-md-0" type="submit">LOGOUT</button>
+    </form> 
+  </div>
+</nav>
+</section> 
+    
+
+
+
+
+<!-- Agrega el código de Bootstrap 5 en el Script -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+
+</body>
+</html>
